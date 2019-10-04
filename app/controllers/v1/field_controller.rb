de8473051b91dff357abd17c81660fe3621b09a2
Iroha71 <y.koba.h1071@outlook.jp>
2019-10-04 23:13:32 +0900

    提案合計数を加算するAPI追加 #3

--- app/controllers/v1/field_controller.rb
+++ app/controllers/v1/field_controller.rb
@@ -1,2 +1,11 @@
 class V1::FieldController < ApplicationController
+    #学科を提案数を増やす
+    def increment_total_num
+        @field = Field.find(params[:id]).plus_total_num()
+        if @field.save!
+            render json: { message: '提案総数更新成功' }
+        else
+            render json: { error: '学科が存在しません' }
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace 'v1' do
     resources :player
-    resources :field
+    resources :field do
+      member do
+        put :increment_total_num
+      end
+    end
   end
 end
