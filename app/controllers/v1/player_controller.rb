213bbab7fb6ade0b25b9039de965f4ff33905ddb
Takaya <mail.1701125@gmail.com>
2019-10-07 16:02:11 +0900

    #6_プレイヤーcreateAPI制作

--- app/controllers/v1/player_controller.rb
+++ app/controllers/v1/player_controller.rb
@@ -1,2 +1,18 @@
 class V1::PlayerController < ApplicationController
+    #プレイヤーを新規登録する
+    def create
+        @player = Player.new(player_params)#player_params＝Postデータをチェックするメソット
+        if @player.save!
+            render json: { message: 'プレイヤー登録成功' }
+        else
+            render json: { error: 'プレイヤー登録失敗' }
+        end
+    end
+
+    private
+    #ストロングパラメータ
+
+    def player_params
+        params.require(:player).permit(:name,:field_id)
+    end
 end
