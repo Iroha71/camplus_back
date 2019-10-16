19a9105060ec4337ed587dd4e1c49164fc0fbd41
Iroha71 <y.koba.h1071@outlook.jp>
2019-10-16 10:34:51 +0900

    Userコントローラ作成 #17

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: { registration: 'auth/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace 'v1' do
     resources :player
